8e2ab9c99d767e652c677ecfb05a5c55e388655b
venicemrl <74256099+venicemrl@users.noreply.github.com>
2024-07-26 09:30:48 +0800

    modified fields on 'contents' table

--- TinyMCE/database/migrations/2024_07_23_021309_create_contents_table.php
+++ TinyMCE/database/migrations/2024_07_23_021309_create_contents_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('contents', function (Blueprint $table) {
             $table->id();
-            $table->string('contents');
+            $table->string('title');
+            $table->string('description');
             $table->timestamps();
         });
     }
